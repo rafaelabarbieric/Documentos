3766e98ae2778a5d8eb95613c767dccac67163cc
rafaelabarbierc <rafaelabarbieric@gmail.com>
2024-07-12 15:11:41 -0300

    FEAT: aprendendo componentes

---
+++ componentes/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue'
+</script>
+
+<template>
+  <h1>Componentes</h1>
+  <hr />
+  <ExpandBox title="Caixa 1" content="Conteúdo secreto da caixa 1" subtitle="Baú 1"/>
+  <expand-box title="Caixa 2" content="Conteúdo secreto da caixa 2" subtitle="Baú 2"/>
+</template>
+
+<style scoped></style>
---
+++ componentes/src/components/ExpandBox.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+import { ref, computed } from 'vue'
+
+const props = defineProps(['title', 'content', 'subtitle'])
+
+const showBox = ref(false)
+
+const textoBotao = computed(() => 
+    showBox.value ? 'Esconder' : 'Mostrar'
+)
+
+</script>
+
+<template>
+
+    <div>
+
+        <button @click="showBox = !showBox">{{ textoBotao }}</button>
+
+        <div v-if="showBox" class="expand-box">
+            <h1>{{ props.title }}</h1>
+            <p>{{ props.content }}</p>
+            <h2 class="color">{{ props.subtitle }}</h2>
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+.expand-box{
+    height: 100%;
+    width: 400px;
+    padding: 20px;
+    border-radius: 10px;
+    border: 1px solid black;
+    box-shadow: 3px 3px 2px 2px gray;
+}
+.color{
+    color: red;
+}
+
+</style>
